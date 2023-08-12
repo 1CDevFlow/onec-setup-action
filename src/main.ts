5037d92e04d828ac4aad6b19379ba809125a2811
IT-Medved <it.medve.dev@yandex.com>
2023-08-12 10:05:41 +0300

    edt
    edt

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as core from '@actions/core'
 import * as cache from '@actions/cache'
 import * as tc from '@actions/tool-cache'
-import { exec } from '@actions/exec'
+import {exec} from '@actions/exec'
 
 async function installOneGet(version: string, platform: string): Promise<void> {
   const key = `oneget----${platform}----${version}`
@@ -31,23 +31,66 @@ async function installOneGet(version: string, platform: string): Promise<void> {
     } else {
       oneGetFolder = await tc.extractTar(onegetPath, gstsrc)
     }
-    core.info(`oneget was extracted`)
-    // await exec.exec(
-    //   `curl -L https://github.com/v8platform/oneget/releases/download/${version}/oneget_${platform}_x86_64.${extension} --output oneget.${extension}`
-    // )
+    core.info(`oneget was extracted ${oneGetFolder} -> ${gstsrc}`)
 
     await cache.saveCache([gstsrc], key)
-    core.addPath(oneGetFolder)
 
     core.info(`New cache created for this key: "${key}"`)
   } else {
     core.info(`Found oneget cache; using it. (key: "${key}")`)
   }
+
+  core.addPath(gstsrc)
+  await exec('chmod', ['+x', `${gstsrc}/oneget`])
 }
+
+
+async function installEDT(version: string, platform: string): Promise<void> {
+  const key = `edt----${platform}----${version}`
+  const gstsrc = '/edt'
+  const cacheKey = await cache.restoreCache([gstsrc], key)
+
+  if (!cacheKey) {
+    core.info(`oneget cache not found; creating a new one. (key: "${key}")`)
+
+    if (platform === 'Windows') {
+    } else {
+    }
+    let onegetPlatform = ''
+    if (platform === 'Windows') {
+      onegetPlatform = 'win'
+    } else {
+      onegetPlatform = 'linux'
+    }
+
+    await exec('oneget',['get',`edt:${onegetPlatform}@${version}`])
+    
+    core.info(`edt was downloaded`)
+
+    let oneGetFolder
+    if (platform === 'Windows') {
+      oneGetFolder = await tc.extractZip(onegetPath, gstsrc)
+    } else {
+      oneGetFolder = await tc.extractTar(onegetPath, gstsrc)
+    }
+    core.info(`oneget was extracted ${oneGetFolder} -> ${gstsrc}`)
+
+    await cache.saveCache([gstsrc], key)
+
+    core.info(`New cache created for this key: "${key}"`)
+  } else {
+    core.info(`Found oneget cache; using it. (key: "${key}")`)
+  }
+
+  core.addPath(gstsrc)
+  await exec('chmod', ['+x', `${gstsrc}/oneget`])
+
+}
+
 async function run(): Promise<void> {
   const platformType = process.platform
   const onegetVersion = 'v0.6.0'
-
+  const edtVersion = '2023.1.1'
   try {
     let platform = ''
 
@@ -70,6 +113,8 @@ async function run(): Promise<void> {
     }
 
     await installOneGet(onegetVersion, platform)
+    await installEDT(edtVersion, platform)
+
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
