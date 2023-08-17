8dffd47d3358a20739ff369dc73aa0bd86cddafd
IT-Medved <it.medve.dev@yandex.com>
2023-08-18 00:40:42 +0300

    edt + platform

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import * as core from '@actions/core'
 import * as cache from '@actions/cache'
 import * as tc from '@actions/tool-cache'
 import {exec} from '@actions/exec'
-
+import * as glob from '@actions/glob'
+import * as io from '@actions/io'
 async function installOneGet(version: string, platform: string): Promise<void> {
   const key = `oneget----${platform}----${version}`
 
   const gstsrc = '/tmp/oneget'
+
   const cacheKey = await cache.restoreCache([gstsrc], key)
 
   if (!cacheKey) {
@@ -19,9 +21,12 @@ async function installOneGet(version: string, platform: string): Promise<void> {
       extension = 'tar.gz'
     }
 
+    const archivePath = `/tmp/oneget.${extension}`
+    await io.rmRF(archivePath)
+
     const onegetPath = await tc.downloadTool(
       `https://github.com/v8platform/oneget/releases/download/${version}/oneget_${platform}_x86_64.${extension}`,
-      `oneget.${extension}`
+      `${archivePath}`
     )
     core.info(`oneget was downloaded`)
 
@@ -32,9 +37,11 @@ async function installOneGet(version: string, platform: string): Promise<void> {
       oneGetFolder = await tc.extractTar(onegetPath, gstsrc)
     }
     core.info(`oneget was extracted ${oneGetFolder} -> ${gstsrc}`)
-
-    await cache.saveCache([gstsrc], key)
-
+    try {
+      await cache.saveCache([gstsrc], key)
+    } catch (error) {
+      if (error instanceof Error) core.info(error.message)
+    }
     core.info(`New cache created for this key: "${key}"`)
   } else {
     core.info(`Found oneget cache; using it. (key: "${key}")`)
@@ -44,53 +51,132 @@ async function installOneGet(version: string, platform: string): Promise<void> {
   await exec('chmod', ['+x', `${gstsrc}/oneget`])
 }
 
-
 async function installEDT(version: string, platform: string): Promise<void> {
   const key = `edt----${platform}----${version}`
-  const gstsrc = '/edt'
+  const gstsrc = '/opt/1C'
+  //const onegetDowloads = 'downloads'
+  const installerPattern = '1ce-installer-cli'
   const cacheKey = await cache.restoreCache([gstsrc], key)
 
   if (!cacheKey) {
-    core.info(`oneget cache not found; creating a new one. (key: "${key}")`)
+    core.info(`edt cache not found; creating a new one. (key: "${key}")`)
 
-    if (platform === 'Windows') {
-    } else {
-    }
     let onegetPlatform = ''
     if (platform === 'Windows') {
       onegetPlatform = 'win'
+    } else if (platform === 'Darwin') {
+      onegetPlatform = 'mac'
     } else {
       onegetPlatform = 'linux'
     }
 
-    await exec('oneget',['get',`edt:${onegetPlatform}@${version}`])
-    
+    try {
+      await exec('oneget', [
+        'get',
+        '--extract',
+        `edt:${onegetPlatform}@${version}`
+      ])
+    } catch (error) {
+      if (error instanceof Error) core.info(error.message)
+    }
+
     core.info(`edt was downloaded`)
 
-    let oneGetFolder
-    if (platform === 'Windows') {
-      oneGetFolder = await tc.extractZip(onegetPath, gstsrc)
-    } else {
-      oneGetFolder = await tc.extractTar(onegetPath, gstsrc)
-    }
-    core.info(`oneget was extracted ${oneGetFolder} -> ${gstsrc}`)
+    const patterns = [`**/${installerPattern}`]
+    const globber = await glob.create(patterns.join('\n'))
+    const files = await globber.glob()
 
-    await cache.saveCache([gstsrc], key)
+    core.info(`finded ${files}`)
+
+    await exec('sudo', [
+      files[0],
+      'install',
+      '--ignore-hardware-checks',
+      '--ignore-signature-warnings'
+    ])
+
+    try {
+      await cache.saveCache([gstsrc], key)
+    } catch (error) {
+      if (error instanceof Error) core.info(error.message)
+    }
 
     core.info(`New cache created for this key: "${key}"`)
   } else {
     core.info(`Found oneget cache; using it. (key: "${key}")`)
   }
+}
+async function installPlatform(
+  version: string,
+  platform: string
+): Promise<void> {
+  const key = `enec----${platform}----${version}`
+  const gstsrc = '/opt/1cv8'
+  //const onegetDowloads = 'downloads'
+  const installerPattern = 'setup-full'
+  const cacheKey = await cache.restoreCache([gstsrc], key)
 
-  core.addPath(gstsrc)
-  await exec('chmod', ['+x', `${gstsrc}/oneget`])
+  if (!cacheKey) {
+    core.info(`onec cache not found; creating a new one. (key: "${key}")`)
 
+    let onegetPlatform = ''
+    if (platform === 'Windows') {
+      onegetPlatform = 'win'
+    } else if (platform === 'Darwin') {
+      onegetPlatform = 'mac'
+    } else {
+      onegetPlatform = 'linux'
+    }
+
+    try {
+      await exec('oneget', [
+        'get',
+        '--extract',
+        `platform:${onegetPlatform}.full.x64@${version}`
+      ])
+    } catch (error) {
+      if (error instanceof Error) core.info(error.message)
+    }
+
+    core.info(`onec was downloaded`)
+
+    const patterns = [`**/${installerPattern}*`]
+    const globber = await glob.create(patterns.join('\n'))
+    const files = await globber.glob()
+
+    core.info(`finded ${files}`)
+
+    await exec('sudo', [
+      files[0],
+      '--mode',
+      'unattended',
+      '--enable-components',
+      'server',
+      '--disable-components',
+      'client-full',
+      'client_thin',
+      'client_thin_fib',
+      'ws'
+    ])
+
+    try {
+      await cache.saveCache([gstsrc], key)
+    } catch (error) {
+      if (error instanceof Error) core.info(error.message)
+    }
+
+    core.info(`New cache created for this key: "${key}"`)
+  } else {
+    core.info(`Found oneget cache; using it. (key: "${key}")`)
+  }
 }
 
-async function run(): Promise<void> {
+export async function run(): Promise<void> {
   const platformType = process.platform
   const onegetVersion = 'v0.6.0'
-  const edtVersion = '2023.1.1'
+  const edtVersion = '2022.2.5'
+  const platformVersion = '8.3.21.1890'
+
   try {
     let platform = ''
 
@@ -114,11 +200,9 @@ async function run(): Promise<void> {
 
     await installOneGet(onegetVersion, platform)
     await installEDT(edtVersion, platform)
-
+    await installPlatform(platformVersion, platform)
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
-
-  await exec('oneget')
 }
